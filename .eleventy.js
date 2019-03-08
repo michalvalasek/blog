f2cd119af9dfe8efb27a2e3b0769ced7a11329ea
Michal Valášek <michal.valasek@gmail.com>
2019-03-08 11:24:38 +0100

    Add weblog and articles archive.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,22 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const CleanCSS = require("clean-css");
+const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
+
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
+  });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    return (n < 0) ? array.slice(n) : array.slice(0, n);
+  });
+
+  // format given string using markdown
+  eleventyConfig.addNunjucksFilter("md", function(string) {
+    let mdlib = markdownIt();
+    return mdlib.renderInline(string);
   });
 }
